d18494305f0a79d69c65412e3eacdfae8a5c34c9
prashant106dhote <118248532+prashant106dhote@users.noreply.github.com>
2023-08-20 16:34:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.to("#text p",{
+ ease:Expo.easeInOut,
+ stagger:1,
+//    duration:2,
+right:0,
+})
+
+    gsap.to("#text p",{
+        delay:1,
+        ease:Expo.easeInOut,
+        stagger:1,
+        // duration:2,
+       right:"-400",
+       })
+       gsap.to("#text p",{
+        delay:1,
+        ease:Expo.easeInOut,
+        stagger:1,
+        // duration:2,
+       right:"-400",
+       })
+   
